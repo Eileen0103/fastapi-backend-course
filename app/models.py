8580ae3a15a5ade3e2eaa17c1a8b1da0c7c203a8
xminhwi <abccccc55@yahoo.com.tw>
2024-12-12 15:42:39 +0800

    Add users table

--- app/models.py
+++ app/models.py
@@ -14,6 +14,6 @@ class User(Base):
     __tablename__="Users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
+    username = Column(String, nullable=False, unique=True) 
+    password = Column(String, nullable=False)               
+    email = Column(String, nullable=False, unique=True)     
